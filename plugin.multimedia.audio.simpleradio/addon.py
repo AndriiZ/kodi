3dcb86d5de3020c683bd1629326572c16afd00fe
AndriiZ <AndriiZ@users.noreply.github.com>
2016-04-09 13:50:03 +0300

    Changed url to local

--- plugin.multimedia.audio.simpleradio/addon.py
+++ plugin.multimedia.audio.simpleradio/addon.py
@@ -15,24 +15,23 @@ addon_url = sys.argv[0]
 
 xbmcplugin.setContent(addon_handle, 'audio')
 
-RADIOS = {'JamFM': {'name': 'JamFM (95.6)', 'thumb': 'http://jamfm.com.ua/i/raw/1/1396270676', 'url': 'http://cast.jamfm.com.ua/jamfm', 'desc': 'Rock station', 'genre': 'Rock'},
-          'ERadio': {'name': 'ЄРадіо', 'url': 'http://etoradio.cc.colocall.com:8500/eradio_hi', 'desc': 'Є! Радіо — хвиля справжньої української музики', 'thumb': 'http://eradio.ua/image/logo.png', 'genre': 'Rock/Pop'},
-          'Hrom': {'name': 'Громадське радіо', 'url': 'http://193.105.70.76:8000/stream', 'thumb': 'http://hromadskeradio.org/sites/default/files/media/brending/hromadskeradio-logo-250x45.png', 'desc': 'слухайте. думайте.', 'fanart': '', 'genre': 'Talk'},
-		  'UR1': {'name': 'Українське радіо', 'thumb': 'http://proradio.org.ua/logos/ur1big.jpg', 'url': 'http://nrcu.gov.ua:8000/ur1-mp3', 'desc': 'Перший Національний - завжди перший!', 'genre': 'Talk'},
-		  'UR2': {'name': 'Радіо Промінь', 'thumb': 'http://proradio.org.ua/logos/prominbig.jpg', 'url': 'http://nrcu.gov.ua:8000/ur2-mp3', 'desc': 'Слухай українське!', 'genre': 'Talk'},
-          'RadioROKS': {'name': 'Радіо РОКС', 'thumb': 'http://www.proradio.org.ua/logos/roksbig.jpg', 'url': 'http://online-radioroks.tavrmedia.ua/RadioROKS', 'desc': 'Рок, тільки рок', 'genre': 'Talk'},
-          'RadioEra': {'name': 'Радіо Ера', 'thumb': 'http://eramedia.com.ua/images/logo_3.png?ua', 'url': 'http://212.26.129.2:8000/era96', 'desc': 'Твій медіа навігатор', 'genre': 'Talk'},
-          'RadioEra': {'name': 'Радіо Ера', 'thumb': 'http://eramedia.com.ua/images/logo_3.png?ua', 'url': 'http://212.26.129.2:8000/era96', 'desc': 'Твій медіа навігатор', 'genre': 'Talk'},
-          'RadioYes': {'name': 'Радіо ЄС', 'thumb':'http://www.proradio.org.ua/logos/biges.gif', 'url': 'http://185.65.245.34:8000/kiev', 'desc': 'Радіо європейської столиці', 'genre':'Music'},
-          'BusinessRadio': {'name': 'Бізнес радіо', 'thumb': 'http://www.proradio.org.ua/logos/business938big.gif','url': 'http://217.20.164.163:8018', 'desc':'Новий формат ваших рішень', 'genre': 'Music'},
-          'UR3': {'name': 'Радіо Культура', 'thumb': 'http://proradio.org.ua/logos/ur3big.jpg', 'url': 'http://nrcu.gov.ua:8000/ur3-mp3', 'desc': 'Канал духовного відродження', 'genre': 'Talk'},
-          'FMAristocrats': {'name': 'Aristocrats FM', 'thumb': 'https://scontent-frt3-1.xx.fbcdn.net/hprofile-xap1/v/t1.0-1/12119090_1705753319656877_247773860828234647_n.png?oh=d8496086fa3e4b5b696bfcdc75fe6a11&oe=568C7D4B', 'url': 'htt\
-p://air.aristocrats.fm:8000/live2', 'genre': 'Misc', 'desc':'Радио Аристократы'},
-          'Hvylia': {'name': 'Радіохвиля', 'url': 'http://radiohvilya.com.ua:8000/radiohvilya?type=.mp3', 'thumb': 'https://scontent-fra3-1.xx.fbcdn.net/hphotos-xta1/t31.0-8/11899777_1457239817916758_8834705431766478969_o.png', 'genre':\
- 'Music', 'desc': 'Слухай серцем'},
-          'Molode': {'name': 'Молоде радіо', 'url': 'http://molode.com.ua:8128/', 'thumb': 'http://molode.com.ua/pics/r01.jpg', 'genre': 'Music', 'desc': '100% сучасної української музики онлайн! Contemporary UA music online!'},
-          'Radio24': {'name': 'Радіо 24', 'url': 'http://icecast.luxnet.ua/radio24', 'thumb': 'http://staticcdn.luxnet.ua/radio24/assets/img/logo-r24.png', 'genre': 'Misc', 'desc': 'Будь собою!'},
-          'Skovoroda': {'name': 'Радіо Сковорода', 'url': 'http://195.248.234.62:8000/radioskovoroda', 'thumb': 'http://radioskovoroda.com/templates/skovoroda/images/logo-smal.png', 'genre': 'Misc', 'desc': ''}
+ROOT = 'http://192.168.1.20:8000/'
+
+RADIOS = {'JamFM': {'name': 'JamFM (95.6)', 'thumb': 'logo/jamfm.png', 'url': 'jamfm', 'desc': 'Rock station', 'genre': 'Rock'},
+          'ERadio': {'name': 'ЄРадіо', 'url': 'eradio', 'desc': 'Є! Радіо — хвиля справжньої української музики', 'thumb': 'logo/eradio.png', 'genre': 'Rock/Pop'},
+          'Hrom': {'name': 'Громадське радіо', 'url': 'hromadske', 'thumb': 'logo/hromadske.png', 'desc': 'слухайте. думайте.', 'fanart': '', 'genre': 'Talk'},
+	  'UR1': {'name': 'Українське радіо', 'thumb': 'logo/ur1.png', 'url': 'ur1', 'desc': 'Перший Національний - завжди перший!', 'genre': 'Talk'},
+	  'UR2': {'name': 'Радіо Промінь', 'thumb': 'logo/ur2.png', 'url': 'ur2', 'desc': 'Слухай українське!', 'genre': 'Talk'},
+          'RadioROKS': {'name': 'Радіо РОКС', 'thumb': 'logo/RadioROKS.jpg', 'url': 'RadioROKS', 'desc': 'Рок, тільки рок', 'genre': 'Talk'},
+          'RadioEra': {'name': 'Радіо Ера', 'thumb': 'logo/radioera.png', 'url': 'RadioEra', 'desc': 'Твій медіа навігатор', 'genre': 'Talk'},
+          'RadioYes': {'name': 'Радіо ЄС', 'thumb':'logos/radioyes.gif', 'url': 'radioyes', 'desc': 'Радіо європейської столиці', 'genre':'Music'},
+          'BusinessRadio': {'name': 'Бізнес радіо', 'thumb': 'logos/businessradio.gif','url': 'BusinessRadio', 'desc':'Новий формат ваших рішень', 'genre': 'Music'},
+	  'UR3': {'name': 'Радіо Культура', 'thumb': 'logo/ur3.png', 'url': 'ur3', 'desc': 'Канал духовного відродження', 'genre': 'Talk'},
+          'FMAristocrats': {'name': 'Aristocrats FM', 'thumb': 'logo/aristocratsFm.png', 'url': 'AristocratsFm', 'genre': 'Misc', 'desc':'Радио Аристократы'},
+          'Hvylia': {'name': 'Радіохвиля', 'url': 'radiohvilya', 'thumb': 'logo/radioHvylya.png', 'genre': 'Music', 'desc': 'Слухай серцем'},
+          'Molode': {'name': 'Молоде радіо', 'url': 'molode', 'thumb': 'logo/molode.jpg', 'genre': 'Music', 'desc': '100% сучасної української музики онлайн! Contemporary UA music online!'},
+          'Radio24': {'name': 'Радіо 24', 'url': 'radio24', 'thumb': 'logo/radio24.png', 'genre': 'Misc', 'desc': 'Будь собою!'},
+          'Skovoroda': {'name': 'Радіо Сковорода', 'url': 'radioskovoroda', 'thumb': 'logo/skovoroda.png', 'genre': 'Misc', 'desc': ''}
           }
 
 def get_stations():
@@ -42,7 +41,7 @@ def get_station(key):
 	return RADIOS[key]
 
 def get_action(url):
-    return '?action=play&source={0}'.format(url)
+    return '?action=play&source={0}{1}'.format(ROOT, url)
 	
 def list_stations():
     playlist = []
@@ -50,15 +49,14 @@ def list_stations():
     for station in stations:
         item = get_station(station)
         url = '{0}{1}'.format(addon_url, get_action(item['url']))
-        list_item = xbmcgui.ListItem(label=item['name'], label2=item['desc'], thumbnailImage=item['thumb'])
+        thumburl = '{0}{1}'.format(ROOT, item['thumb'])
+        list_item = xbmcgui.ListItem(label=item['name'], label2=item['desc'], thumbnailImage=thumburl)
         list_item.setProperty('IsPlayable', 'true')
         title = '{0}. {1}'.format(item['name'], item['desc'])
         list_item.setInfo(type='music', infoLabels = {'title': title, 'genre': item['genre']})
         playlist.append((url,list_item,False,item['url']))
     xbmcplugin.addDirectoryItems(addon_handle,playlist,1)
     xbmcplugin.addSortMethod(addon_handle,xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    if xbmc.Player().isPlaying() != True:
-        xbmc.Player().play('http://cast.jamfm.com.ua/jamfm')
     xbmcplugin.endOfDirectory(addon_handle)
 
 def play_radio(radio):
